1ece5fedec712523170981189c0ee0786d694dfa
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2022-07-12 16:55:12 -0300

    Add sprites and animations
    sprites adicionadas e as animações com limites na tela

--- script.js
+++ script.js
@@ -11,20 +11,54 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor(){
-        this.width = 100;
-        this.height = 50;
+
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier ;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
+        this.markedForDeletion = false;
+
+        this.image = new Image();
+        this.image.src = 'raven.png';
+
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50;
+        
     }
 
-    update(){
+    update(deltatime){
+        if(this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
+        this.y -= this.directionY;
+        if(this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        
+        if(this.timeSinceFlap > this.flapInterval){
+            if(this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;
+        }
+
+        
     }
 
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(
+            this.image, 
+            this.frame * this.spriteWidth, 0, 
+            this.spriteWidth, this.spriteHeight,
+            this.x, this.y, 
+            this.width, this.height);
     }
 }
 
@@ -42,8 +76,10 @@ function animate(timestamp){
         ravens.push(new Raven());
         timeToNextRaven = 0; 
     }
-    [...ravens].forEach(object => object.update()); //modifica a posição do objeto
+    [...ravens].forEach(object => object.update(deltatime)); //modifica a posição do objeto
     [...ravens].forEach(object => object.draw()); //desenha o objeto na tela
+    ravens = ravens.filter(object => !object.markedForDeletion);
+    console.log(ravens);
 
     requestAnimationFrame(animate);    
 }
