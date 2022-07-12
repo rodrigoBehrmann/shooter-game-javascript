3b6693044e7ee816a1e194427a38886cf3833527
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2022-07-12 15:29:13 -0300

    Update script.js
    funcao animate criada, uso do timestamp para gerar o objeto a  na tela

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContexto('2d');
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height = window.innerheight;
+canvas.height = window.innerHeight;
 
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
+
+let ravens = [];
+class Raven {
+    constructor(){
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 5 + 3;
+        this.directionY = Math.random() * 5 - 2.5;
+    }
+
+    update(){
+        this.x -= this.directionX;
+    }
+
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const raven = new Raven();
+
+//frame by frame update with timestamp
+function animate(timestamp){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+    timeToNextRaven += deltatime;
+
+    if(timeToNextRaven > ravenInterval){
+        ravens.push(new Raven());
+        timeToNextRaven = 0; 
+    }
+    [...ravens].forEach(object => object.update()); //modifica a posição do objeto
+    [...ravens].forEach(object => object.draw()); //desenha o objeto na tela
+
+    requestAnimationFrame(animate);    
+}
+
+animate(0);
